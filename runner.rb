b51270dc6bd342bcadfa7311d716379513873d5a
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-17 19:18:02 -0600

    Create my branch

---
+++ runner.rb
@@ -0,0 +1,14 @@
+require './lib/stat_tracker'
+
+game_path = './data/games.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/game_teams.csv'
+
+locations = {
+  games: game_path,
+  teams: team_path,
+  game_teams: game_teams_path
+}
+
+stat_tracker = StatTracker.from_csv(locations[:games])
+p CSV.parse(File.read(stat_tracker.data), headers: true).by_col[1]
