b9ce53bc82dc8f0899d6ca37e6351631caaddefd
Corey Carter <crc2231@gmail.com>
2020-07-17 22:33:12 -0600

    Create highest_total_score method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,10 @@
 require 'CSV'
 
 class StatTracker
-  
+
   attr_reader :data
+
+
   def self.from_csv(data)
     StatTracker.new(data)
   end
@@ -10,5 +12,17 @@ class StatTracker
   def initialize(data)
     @data = data
   end
+
+  def highest_total_score
+    games_table = CSV.parse(File.read(self.data), headers: true).by_row
+    result = Hash.new{|hash,key| hash[key] = []}
+    games_table.each do |game|
+        result[game[0]] << game[6].to_i
+    end
+    result.transform_values! {|score| score.sum}
+
+    result.values.max
+  end
+
 
 end
