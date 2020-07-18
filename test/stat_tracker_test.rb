03b02743dbb062ba99791a93fd57aa6d04b1bee0
Corey Carter <crc2231@gmail.com>
2020-07-17 22:32:52 -0600

    Create highest_total_score_test

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,11 +1,37 @@
 require "./test/test_helper.rb"
+### Refactor/remove
 class StatTrackerTest < MiniTest::Test
 
+
+  def setup
+    ################## Pulled from Runner.rb
+    game_path = './data/games_sample.csv'
+    team_path = './data/teams_sample.csv'
+    game_teams_path = './data/game_teams_sample.csv'
+
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    #######################
+  end
+
   def test_it_exists
-    stattracker1 = StatTracker.new
+    stattracker1 = StatTracker.new(@locations)
     assert_instance_of StatTracker, stattracker1
   end
 
-  
+  def test_highest_total_score
+    stattracker1 = StatTracker.new(@locations[:game_teams])
+
+    assert_equal 5, stattracker1.highest_total_score
+  end
+
+
+
+
+
+
 
 end
