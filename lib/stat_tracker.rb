63e4ea2a13ac0d400ca1f52125b244cd994a8c11
Corey Carter <crc2231@gmail.com>
2020-07-18 11:15:25 -0600

    Refactor highest/lowest_total_score for SRP

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -13,21 +13,35 @@ class StatTracker
     @data = data
   end
 
-  def game_total_score
-    games_table = CSV.parse(File.read(self.data), headers: true).by_row
-    result = Hash.new{|hash,key| hash[key] = []}
-    games_table.each do |game|
-        result[game[0]] << game[6].to_i
-    end
-    result.transform_values! {|score| score.sum}
+  def data_by_row
+    CSV.parse(File.read(self.data), headers: true).by_row
   end
 
+  def goals_by_game_id
+    result = Hash.new{|hash,key| hash[key] = []}
+    data_by_row.each do |game|
+      result[game[0]] << game[6].to_i
+    end
+    result
+  end
+
+  def game_total_score
+    goals_by_game_id.transform_values! {|score| score.sum}
+  end
+
+##### Need to return team name
   def highest_total_score
     game_total_score.values.max
   end
 
+  ##### Need to return team name
   def lowest_total_score
     game_total_score.values.min
+  end
+
+##### Have to break out by key in Self
+  def percentage_home_wins(team)
+    data_by_row = CSV.parse(File.read(self.data), headers: true).by_row
   end
 
 
