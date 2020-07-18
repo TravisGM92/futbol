f19552cf78d39896336232e8191e918f1b5dd24f
Corey Carter <crc2231@gmail.com>
2020-07-17 22:42:11 -0600

    Add lowest_total_score method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -13,15 +13,21 @@ class StatTracker
     @data = data
   end
 
-  def highest_total_score
+  def game_total_score
     games_table = CSV.parse(File.read(self.data), headers: true).by_row
     result = Hash.new{|hash,key| hash[key] = []}
     games_table.each do |game|
         result[game[0]] << game[6].to_i
     end
     result.transform_values! {|score| score.sum}
+  end
 
-    result.values.max
+  def highest_total_score
+    game_total_score.values.max
+  end
+
+  def lowest_total_score
+    game_total_score.values.min
   end
 
 
