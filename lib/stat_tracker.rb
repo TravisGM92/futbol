826e772ccd1e1cd2746eb6864727865339d7f420
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-19 08:14:04 -0600

    Change test of display info to match spec harness

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,26 +2,55 @@ require 'CSV'
 
 class StatTracker
 
-  attr_reader :data
+  attr_reader :data, :seasons, :game_team_rows, :game_rows, :team_rows, :team_cols, :body
   def self.from_csv(data)
     StatTracker.new(data)
   end
 
   def initialize(data)
     @data = data
+    @game_team_rows = CSV.parse(File.read(@data[:game_teams]))
+    @game_rows = CSV.parse(File.read(@data[:games]))
+    @seasons = CSV.parse(File.read(@data[:games]), headers: true).map do |col|
+      col[1]
+    end.uniq.sort!
+    @headers = CSV.parse(File.read(@data[:teams]),
+      headers: false)[0]
+    @body = File.read(@data[:teams])
+    @team_cols = CSV.parse(File.read(@data[:teams]), headers: false)
   end
 
   def team_info(id)
     hash = {}
-    CSV.parse(File.read(@data[:teams]),
-    headers: true).select{ |col| col[0] == "#{id}"}.flat_map do
-      |row| hash[id] = [row[1], row[2], row[3], row[-1]]
+    csv = CSV.new(@body, :headers => true, :header_converters => :symbol)
+    teams_hash = csv.to_a.map{ |row| row.to_hash}
+    teams_hash.find{ |hash| hash[:team_id] == "#{id}"}
+  end
+
+    def best_season(id)
+      games_played = @game_team_rows.select{ |row| row[1] == "#{id}"}
+      games_won = games_played.select{ |row| row[3] == "WIN"}
+      games_won_ids = games_won.map{ |row| row[0]}
+      games_won_games = games_won_ids.map{ |x| @game_rows.select{ |row| row[0] == x}}.map{ |x| x.flatten}
+      season_20122013 = games_won_games.select{ |row| row[1] == "20122013"}.length
+      season_20132014 = games_won_games.select{ |row| row[1] == "20132014"}.length
+      season_20142015 = games_won_games.select{ |row| row[1] == "20142015"}.length
+      season_20152016 = games_won_games.select{ |row| row[1] == "20152016"}.length
+      season_20162017 = games_won_games.select{ |row| row[1] == "20162017"}.length
+      season_20172018 = games_won_games.select{ |row| row[1] == "20172018"}.length
+      all_seasons_results = [season_20122013, season_20132014, season_20142015, season_20152016, season_20162017, season_20172018]
+      if all_seasons_results[0] == all_seasons_results.sort[-1]
+        "20122013"
+      elsif all_seasons_results[1] == all_seasons_results.sort[-1]
+        "20132014"
+      elsif all_seasons_results[2] == all_seasons_results.sort[-1]
+        "20142015"
+      elsif all_seasons_results[3] == all_seasons_results.sort[-1]
+        "20152016"
+      elsif all_seasons_results[4] == all_seasons_results.sort[-1]
+        "20162017"
+      elsif all_seasons_results[5] == all_seasons_results.sort[-1]
+        "20172018"
+      end
     end
-    hash
   end
-
-  def best_season(id)
-
-  end
-
-end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -2,15 +2,19 @@ require "./test/test_helper.rb"
 class StatTrackerTest < MiniTest::Test
 
   def test_it_exists
-    stattracker1 = StatTracker.new('./data/dummy_data.csv')
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    stattracker1 = StatTracker.from_csv(locations)
+
+
     assert_instance_of StatTracker, stattracker1
-  end
-
-  def test_it_can_display_data
-    stat_tracker1 = StatTracker.new('./data/dummy_data.csv')
-
-    expected = "./data/dummy_data.csv"
-    assert_equal stat_tracker1.data, expected
   end
 
   def test_it_can_display_team_name_with_team_ID
@@ -25,26 +29,9 @@ class StatTrackerTest < MiniTest::Test
     }
 
     stat_tracker = StatTracker.from_csv(locations)
-    expected = {2=>["22", "Seattle Sounders FC", "SEA", "/api/v1/teams/2"]}
+    expected = {:team_id=>"2", :franchiseid=>"22", :teamname=>"Seattle Sounders FC", :abbreviation=>"SEA", :stadium=>"Centruy Link Field", :link=>"/api/v1/teams/2"}
 
     assert_equal stat_tracker.team_info(2), expected
-  end
-
-  def test_it_can_display_best_season_for_a_team
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-
-    locations =  {
-      games: game_path,
-      teams: team_path,
-      game_tams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-    expected = 2
-
-    assert_equal stat_tracker.best_season(3), expected
   end
 
 end
