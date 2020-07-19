22d2514ecfaedcfec3bfe91ee910a36f89d14201
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-19 10:30:36 -0600

    Clean up best season method a bit more

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -24,16 +24,14 @@ class StatTracker
 
   def best_season(id)
     games_played = @game_team_rows.select{ |row| row[1] == "#{id}"}
-    games_won = games_played.select{ |row| row[3] == "WIN"}
-    games_won_ids = games_won.map{ |row| row[0]}
-    games_won_games = games_won_ids.map{ |x| @game_rows.select{ |row| row[0] == x}}.map{ |x| x.flatten}
-    season_20122013 = games_won_games.select{ |row| row[1] == "20122013"}.length
-    season_20132014 = games_won_games.select{ |row| row[1] == "20132014"}.length
-    season_20142015 = games_won_games.select{ |row| row[1] == "20142015"}.length
-    season_20152016 = games_won_games.select{ |row| row[1] == "20152016"}.length
-    season_20162017 = games_won_games.select{ |row| row[1] == "20162017"}.length
-    season_20172018 = games_won_games.select{ |row| row[1] == "20172018"}.length
-    all_seasons_results = [season_20122013, season_20132014, season_20142015, season_20152016, season_20162017, season_20172018]
+    games_won = games_played.select{ |row| row[3] == "WIN"}.map{ |row| row[0]}
+    games_won_games = games_won.map{ |x| @game_rows.select{ |row| row[0] == x}}.map{ |x| x.flatten}
+    all_seasons_results = [games_won_games.select{ |row| row[1] == "20122013"}.length,
+    games_won_games.select{ |row| row[1] == "20132014"}.length,
+    games_won_games.select{ |row| row[1] == "20142015"}.length,
+    games_won_games.select{ |row| row[1] == "20152016"}.length,
+     games_won_games.select{ |row| row[1] == "20162017"}.length,
+     games_won_games.select{ |row| row[1] == "20172018"}.length]
       if all_seasons_results[0] == all_seasons_results.sort[-1]
         "20122013"
       elsif all_seasons_results[1] == all_seasons_results.sort[-1]
